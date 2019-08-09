d419df91d0bce782d20e2fa0ead62f0fc129bd16
shawn <shawnantonucci2018@gmail.com>
2019-08-09 13:46:08 -0700

    navbar implimented

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Dashboard = () => {
     return (
         <div>
+            <Navbar />
             Dashboard
         </div>
     )
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Menu } from "semantic-ui-react";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
-}
+    const [activeItem, setActiveItem] = useState(null);
 
-export default Navbar
+    const handleItemClick = (e, { name }) => setActiveItem(name);
+
+    return (
+        <Menu>
+            <Menu.Item
+                name="home"
+                active={activeItem === "home"}
+                onClick={handleItemClick}
+            >
+                Home
+            </Menu.Item>
+
+            <Menu.Item
+                name="gallery"
+                active={activeItem === "gallery"}
+                onClick={handleItemClick}
+            >
+                Gallery
+            </Menu.Item>
+
+            <Menu.Menu position="right">
+                <Menu.Item
+                    name="cart"
+                    active={activeItem === "cart"}
+                    onClick={handleItemClick}
+                >
+                    Cart
+                </Menu.Item>
+
+                <Menu.Item
+                    name="checkout"
+                    active={activeItem === "checkout"}
+                    onClick={handleItemClick}
+                >
+                    Checkout
+                </Menu.Item>
+            </Menu.Menu>
+        </Menu>
+    );
+};
+
+export default Navbar;
